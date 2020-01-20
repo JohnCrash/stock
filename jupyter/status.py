f21cf66fa7503a54a946c550851f903c719514c7
john1v7 <udsn2@sina.com>
2020-01-20 22:28:00 +0800

    progress

--- jupyter/status.py
+++ jupyter/status.py
@@ -11,8 +11,9 @@ from datetime import date
 from IPython.display import display
 import ipywidgets as widgets
 from ipywidgets import Layout, Button, Box
-from datetime import date,timedelta
+from datetime import date,datetime,timedelta
 import matplotlib.pyplot as plt
+import math
 
 PROD = 40
 #见数据插入到company_status表
@@ -73,13 +74,14 @@ def update_company_status_delta_data(idk,idd):
     insert_company_status(k[PROD+1:],vma20[PROD+1:],energy[PROD+1:],volumeJ[PROD+1:],idd[PROD+1:])
 
 #可以从一个起点日期使用增量进行更新，或者更新全部数据
-def update_status_begin(beginday,isall=False):
+def update_status_begin(beginday,isall,progress):
     if isall:
         rs = stock.query("""select id,date,volume,close,macd from kd_xueqiu where date>'%s'"""%(beginday))
+        progress(30)
     else:
         #需要提前20个交易日的数据
         lastdays = stock.query("select date from kd_xueqiu where id=8828 and date>'2019-11-01' order by date desc")
-      
+        progress(30)
         #从beginday前面在增加20个交易日
         for i in range(len(lastdays)):
             if lastdays[i][0] == beginday:
@@ -98,40 +100,55 @@ def update_status_begin(beginday,isall=False):
         #0 id,1 volume,2 close,3 macd
         idk[key].append([rs[i][0],rs[i][2],rs[i][3],rs[i][4]])
         idd[key].append(rs[i][1])
-
+    progress(40)
+    count = 0
     if isall:
         for key in idk:
             update_company_status_all_data(idk[key],idd[key])
+            count+=1
+            progress(math.floor(60*count/len(idk))+40)
     else:
         for key in idk:
             update_company_status_delta_data(idk[key],idd[key])
+            count+=1
+            progress(math.floor(60*count/len(idk))+40)
 
 #更新company_status表
-def update_status():
+def update_status(progress):
     lastday = stock.query('select date from company_status where id=8828 order by date desc limit 1')
+    progress(10)
     if len(lastday)==1:
         kdd = stock.query('select date from kd_xueqiu where id=8828 order by date desc limit 1')
+        progress(20)
         if lastday[0][0] != kdd[0][0]:
-            update_status_begin(lastday[0][0])
+            update_status_begin(lastday[0][0],False,progress)
     else:
-        update_status_begin('2010-1-2',True)
+        update_status_begin('2010-1-2',True,progress)
     stock.closedb()
+    progress(100)
 
 #更新company_status_week表
-def update_status_week():
+def update_status_week(progress):
     lastupdate = stock.query('select status_week_update from data')
+    progress(5)
     lastday = stock.query('select date from kd_xueqiu where id=8828 order by date desc limit 1')
+    progress(10)
     if lastupdate[0][0] == lastday[0][0]:
+        progress(100)
         return #已经更新了
     alldate = stock.query("select date from company_status_week where id=8828 order by date desc")
+    progress(20)
     if len(alldate)==0:
         drs = stock.query("select id,date,volume,close from kd_xueqiu where date>'2010-1-2'")
+        progress(50)
         wrs = [] #没有数据
     else:
         #从日线数据中取出未更新部分数据
         drs = stock.query("select id,date,volume,close from kd_xueqiu where date>='%s'"%(alldate[1][0])) #最近两个星期的日线数据
+        progress(40)
         #从周状态中取出一部分历史数据
         wrs = stock.query("select id,date,volume,close from company_status_week where date>='%s'"%(alldate[PROD+1][0])) #最近40天的周线数据
+        progress(50)
     
     #按个股进行重新存储idk[id] = 该id的k线, idd[id] = 该id的日期数组
     idk = {} #
@@ -144,6 +161,7 @@ def update_status_week():
         #volume,open,high,low,close
         idk[key].append([drs[i][2],0,0,0,drs[i][3]])
         idd[key].append([drs[i][1]])
+    progress(60)
     wdk = {} #
     wdd = {}
     for i in range(len(wrs)):
@@ -154,7 +172,7 @@ def update_status_week():
         #id,volume,close
         wdk[key].append([key,wrs[i][2],wrs[i][3]])
         wdd[key].append([wrs[i][1]])
-    
+    progress(70)
     for key in idk:
         k = idk[key]
         d = idd[key]
@@ -180,8 +198,8 @@ def update_status_week():
                     wk[-1][2] = nwk[i][4]#close
                     needappend = True
                 elif needappend:
-                    wd += nwd[i]
-                    wk += [key,nwk[i][0],nwk[i][4]]
+                    wd += [nwd[i]]
+                    wk += [[key,nwk[i][0],nwk[i][4]]]
             #将id,volume,close转换为volume,open,high,low,close
             wwk = []
             for i in range(len(wk)):
@@ -200,9 +218,11 @@ def update_status_week():
         #将周数据更新到company_status_week表中
         #相等部分更新，后面部分插入
         update_company_status_week(key,WK,macd,volumema20,energy,volumeJ,wd,bi)
+    progress(90)
     #全部更新完成写入最新的更新日期
     stock.execute("update data set status_week_update='%s' where id=1"%(lastday[0][0]))
     stock.closedb()
+    progress(100)
 
 """
 用于判断是不是有崛起迹象
@@ -220,13 +240,13 @@ def isRasing(a,company,istoday):
     return False
 
 #找到有崛起继续的个股id , dd是搜索指定日期
-def searchRasingCompanyStatus(dd,period,cb,id2companys):
+def searchRasingCompanyStatus(dd,period,cb,id2companys,progress):
     if period=='d':
         db = 'company_status'
     else:
         db = 'company_status_week'
     lastday = stock.query("""select date from %s where id=8828 order by date desc limit 50"""%(db))
-    
+    progress(10)
     endDate = None
     beginDate = None
     istoday = False
@@ -239,34 +259,70 @@ def searchRasingCompanyStatus(dd,period,cb,id2companys):
         endDate = lastday[0][0]
         beginDate = lastday[40][0]
         istoday = xueqiu.isTransTime()
-    cs = np.array(stock.query("""select id,close,volume,volumema20,macd,energy,volumeJ from %s where date>='%s' and date<='%s'"""%(db,stock.dateString(beginDate),stock.dateString(endDate))))
+    #技能根据id将加载分成9份
+#使用分布加载将大幅度增加耗时    
+#    css = []
+#    t0 = datetime.today()
+#    for i in range(1,9):
+#        progress(i*10)
+#        css+=stock.query("""select id,close,volume,volumema20,macd,energy,volumeJ from %s where date>='%s' and date<='%s' and id>=%d and id<%d"""%(db,stock.dateString(beginDate),stock.dateString(endDate),1000*(i-1),1000*i))
+#    print(datetime.today()-t0)
+    t0 = datetime.today()
+    def progressCB(i):
+        progress(i*10)
+    css=stock.queryProgress("""select id,close,volume,volumema20,macd,energy,volumeJ from %s where date>='%s' and date<='%s'"""%(db,stock.dateString(beginDate),stock.dateString(endDate)),9,progressCB)
+    print(datetime.today()-t0)
+    cs = np.array(css)
+    progress(90)
     stock.closedb()
     idds = {}
     for i in range(len(cs)):
         if cs[i][0] not in idds:
             idds[cs[i][0]] = []
         idds[cs[i][0]].append(cs[i])
+    progress(95)
     rasing = []
     for c in idds:
         if len(idds[c])>=3 and c in id2companys and cb(idds[c],id2companys[c],istoday):
             rasing.append(int(c))
+    progress(100)
     return rasing
 
 """
 按分类列出崛起的股票的数量与列表
 """
 def RasingCategoryList(period='d',cb=isRasing):
-    if period=='d':
-        update_status() #更新公司日状态
-    else:
-        update_status_week()  #更新公司周状态
-    output = widgets.Output()    
-    items_layout = Layout( width='auto')     # override the default width of the button to 'auto' to let the button grow
+    output = widgets.Output()
+    output2 = widgets.Output()
     box_layout = Layout(display='flex',
                         flex_flow='wrap',
                         align_items='stretch',
                         border='solid',
                         width='100%')
+    progress_layout = Layout(display='flex',
+                        flex_flow='wrap',
+                        width='100%')                        
+    progress = widgets.IntProgress(value=0,
+    min=0,max=100,step=1,
+    description='Loading:',
+    bar_style='',
+    orientation='horizontal',layout=progress_layout)
+    def progressCallback(p):
+        nonlocal progress
+        if p < 0:
+            p = 0
+        if p > 100:
+            p = 100
+        progress.value = p
+        if p == 100:
+            progress.close()
+    display(progress,output2)
+
+    if period=='d':
+        update_status(progressCallback) #更新公司日状态
+    else:
+        update_status_week(progressCallback)  #更新公司周状态
+
     #可以提前准备的数据
     categorys = stock.query("""select id,name from category""")
     companys = stock.query("""select company_id,code,name,category,ttm,pb from company_select""")
@@ -275,7 +331,16 @@ def RasingCategoryList(period='d',cb=isRasing):
         id2companys[c[0]] = c
 
     def onCatsList(E):
-        rasing = searchRasingCompanyStatus(E.description,period,cb,id2companys)
+        nonlocal progress
+        progress = widgets.IntProgress(value=0,
+            min=0,max=100,step=1,
+            description='Loading:',
+            bar_style='',
+            orientation='horizontal',layout=progress_layout)
+        with output2:
+            display(progress)            
+        progressCallback(0)
+        rasing = searchRasingCompanyStatus(E.description,period,cb,id2companys,progressCallback)
         cats = {}
         rasingCompany = []
         for c in companys:
@@ -317,8 +382,8 @@ def RasingCategoryList(period='d',cb=isRasing):
     items = []
 
     dates = stock.query('select date from %s where id=8828 order by date desc limit 10'%('company_status' if period=='d' else 'company_status_week'))
-    if period=='d':
-        today = date.today()
+    today = date.today()
+    if period=='d' and dates[0][0] != today:
         #如果今天是一个交易日，并且不在数据库中，那么从雪球直接下载数据
         but = widgets.Button(
             description=str(today),
@@ -334,5 +399,6 @@ def RasingCategoryList(period='d',cb=isRasing):
             button_style='')
         but.on_click(onCatsList)
         items.append(but)
+
     box = Box(children=items, layout=box_layout)
     display(box,output)
