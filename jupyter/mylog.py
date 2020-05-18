e13f8f749b1a2687af987b87d29f0dd6d509d154
john1v7 <udsn2@sina.com>
2020-05-18 22:24:49 +0800

    log

--- jupyter/mylog.py
+++ jupyter/mylog.py
@@ -1,9 +1,10 @@
 import logging
+import config
 
 def init(filename,name='mylog'):
     log = logging.getLogger(name)
     log.setLevel(level = logging.INFO)
-    hd = logging.FileHandler('d:/source/stock/jupyter/log/'+filename)
+    hd = logging.FileHandler(config.log_dir+filename)
     hd.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     hd.setFormatter(formatter)
