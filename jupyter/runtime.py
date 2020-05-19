0a287d32ff7b48351e29395cece8d17d7920628f
john1v7 <udsn2@sina.com>
2020-05-19 12:46:54 +0800

    r

--- jupyter/runtime.py
+++ jupyter/runtime.py
@@ -4,44 +4,52 @@ from datetime import datetime
 import time
 import subprocess
 import threading
+import platform
 import config
+import mylog
 #import os
+log = mylog.init('runtime.log',name='runtime')
+download_done_day = -1
+def process(i):
+    pass
 
 def jupyter():
     subprocess.run(['jupyter','lab'])
 
-#def jupyter_https():
-#    subprocess.run(['jupyter','lab'],cwd='d:\\source\\stock\\jupyter\\config')
-threading.Thread(target=jupyter).start()
-#threading.Thread(target=jupyter_https).start()
+if platform.platform()[:7]=='Windows':
+    print("启动jupyter lab")
+    threading.Thread(target=jupyter).start()
 
-t = datetime.today()
-while t.hour<15:
-    try:
-        xueqiu.updateAllRT()
-        time.sleep(1)
-        t = datetime.today()
-    except Exception as e:
-        print(e)
-def process(i):
-    pass
-print("5分钟后开始更新数据库...")
 while True:
-    if t.hour==15 and t.minute==5:
+    t = datetime.today()
+    if t.hour>=9 and t.hour<15:
+        try:
+            xueqiu.updateAllRT()
+            time.sleep(1)
+        except Exception as e:
+            log.error(str(e))
+            print(e)
+    elif t.hour==15 and t.day!=download_done_day:
+        print("5分钟后开始更新数据库...")
+        time.sleep(5*60)
         print("保存资金流向到数据库...")
-        xueqiu.sinaFlowRT()
-        status.saveflow()      
+        try:
+            xueqiu.sinaFlowRT()
+            status.saveflow()      
+        except Exception as e:
+            log.error(str(e))
+            print(e)
         print("开始从雪球下载数据")
         for i in range(3):
             r = subprocess.run(['node',config.download_js])
             if r.returncode==0:
+                log.info("done")
                 break
+            else:
+                log.warning("%s下载出现问题"%(config.download_js))
         print("开始更新数据库...")
         status.update_status(process)
         print("更新完成。")          
-        break
-    elif t.hour==15:
-        time.sleep(1)
-        t = datetime.today()
+        download_done_day = t.day
     else:
-        break
+        time.sleep(10)
